89b79788c67204158af44a65dc39a86e027c2bec
Diego Moreira <diego@MacBook-Pro.local>
2019-07-29 10:24:23 -0300

    Mixin classes

---
+++ exercicios04.dart
@@ -0,0 +1,27 @@
+void main() {
+  Carro c1 = Carro("Fusca");
+  c1.acelerar(100);
+  c1.abastecer(50);
+}
+
+class Carro extends Automovel with Combustivel {
+  String nome;
+  
+  Carro(this.nome);
+  
+  String toString(){
+    return "Carro $nome";
+  }
+}
+
+class Automovel {
+  void acelerar(int velocidade) {
+    print("Acelerando a $velocidade km/h ");
+  }
+}
+
+class Combustivel{
+  abastecer(int qtde){
+    print("abastecendo $qtde litros");
+  }
+}
